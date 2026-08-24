823612a50a787507d1aaffff5479b3003b9093c7
Gorkem97 <76035137+Gorkem97@users.noreply.github.com>
2026-08-24 15:01:33 +0300

    Revert "hadi bakalım"
    This reverts commit 41c0f140b82a54e462ef15768e3d4346a3ecc8fc.

--- fix_videos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git show "5ff5188^:public/greenkids/Ekran Kaydı 2026-08-23 22.02.05-Apple Devices HD (Most Compatible).m4v" > public/videos/greenmate-session1.mp4
-git show "5ff5188^:public/greenkids/Ekran Kaydı 2026-08-23 22.04.15-Apple Devices HD (Most Compatible).m4v" > public/videos/greenmate-session2.mp4
-git show "5ff5188^:public/Jam titles/Ekran Kaydı 2026-08-23 22.24.31-Apple Devices HD (Most Compatible).m4v" > public/videos/jam-session1.mp4
-git show "5ff5188^:public/Jam titles/jam-session2.mp4" > public/videos/jam-session2.mp4
-git show "5ff5188^:public/Jam titles/trapped.mp4" > public/videos/jam-trapped.mp4
-chmod +x fix_videos.sh
-./fix_videos.sh
-rm fix_videos.sh
